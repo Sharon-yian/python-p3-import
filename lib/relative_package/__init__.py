718836e30a61ee05590a537510159deb37a0403c
Sharon Soyian Lenjir <sharon.lenjir@student.moringaschool.com>
2025-11-26 03:01:35 +0300

    add missing __init__.py for relative_package

---
+++ lib/relative_package/__init__.py
@@ -0,0 +1,5 @@
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown" # package is not installed
